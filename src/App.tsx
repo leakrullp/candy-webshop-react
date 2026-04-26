0da69841b92ca068981a80ed87b52a58e0f4e3f7
Christina Polyzogopoulou <christinapol09@gmail.com>
2026-04-26 16:25:46 +0200

    Created ProductsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./index.css";
 import ProductCard from "./components/ProductCard";
 import BasketItem from "./components/BasketItem";
+import ProductsPage from "./pages/ProductsPage";
 import { type Product } from "./services/productsService";
 
 function App() {
@@ -35,11 +33,6 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
           <h1>Get started</h1>
           <p>
@@ -65,6 +58,10 @@ function App() {
         onRemove={() => {}}
         onUpdateQuantity={() => {}}
       />
+    </section>
+    <section>
+      <h2>Products Page Test</h2>
+      <ProductsPage />
     </section>
     </>
   );
