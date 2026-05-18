63904f4812c0a6aebb712607c7e9b3e97ec2cde5
kdre <kdre@itu.dk>
2026-05-18 10:11:01 +0200

    Display name (not finished) + small fixes

--- src/pages/FrontPage/FrontPage.tsx
+++ src/pages/FrontPage/FrontPage.tsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from "react";
 import type { Product } from "../../types";
 import { ProductBanner } from "../../components";
 import { fetchProducts } from "../../services/productsService";
+import Carousel from "../../components/Carousel";
+import useDisplayName from "../../utils/useDisplayName";
+
 
 export default function FrontPage() {
   //fetch all products once for this page instead of once per ProductBanner
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const name = useDisplayName();
 
   useEffect(() => {
     fetchProducts()
@@ -18,9 +22,14 @@ export default function FrontPage() {
 
   return (
     <>
+    <main className="container mt-5"></main>
       {error && <p>{error}</p>}
       {!loading && (
         <>
+        <header className="text-center mb-5"></header>
+        <h1 className="display-4 fw-bold" id = "welcome-header">{name ? `Welcome to a world of candy, ${name}!!` : "Welcome to a world of candy"}</h1>
+  
+        <Carousel />
           <ProductBanner
             products={products}
             title="On sale"
