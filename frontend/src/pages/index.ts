2ec956b0bcabde5c00cfd0c24de23dbc1ccee8eb
leakrullp <leakrull@proton.me>
2026-05-19 13:00:40 +0200

    Pass products as props to ProductPage - make ProductPage utilize custom useProducts hook - make LoadProductsProps into global type and move into type index - create index for custom hooks and fix imports - add new components and pages to their indices

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,5 +2,7 @@
 export { default as FrontPage } from "./FrontPage/FrontPage";
 export { default as BasketPage } from "./BasketPage";
 export { default as ProductsPage } from "./ProductsPage";
+export { default as ProductDetailPage } from "./ProductDetailPage";
+export { default as CountryPage } from "./CountryPage";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
