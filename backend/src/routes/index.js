e26a04a86487154d7cba6e7a327da6a44794fad5
kdre <kdre@itu.dk>
2026-05-27 11:31:09 +0200

    Fixing index for routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import productRouter from "./productRoutes.js";
 import basketRouter from "./basketRoutes.js";
 import customerRouter from "./customerRoutes.js";
+import categoryRouter from "./categoryRoutes.js";
 
 const router = express.Router();
 
 router.use("/products", productRouter);
 router.use("/baskets", basketRouter);
 router.use("/customers", customerRouter);
+router.use("/categories", categoryRouter);
 
 export default router;
